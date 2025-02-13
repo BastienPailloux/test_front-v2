ccab177bd03e360e4a9786a102dadbab0659680a
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-02-13 11:06:14 +0100

    fetchTasks created in store

--- stores/tasks.ts
+++ stores/tasks.ts
@@ -1,5 +1,19 @@
 import { defineStore } from 'pinia'
 
+import { useApi } from '~/composables/useApi.composable'
+
+import type { Task } from '~/types/Task'
+
 export const useTasksStore = defineStore('tasks', () => {
-  return {}
+  const api = useApi()
+  const tasks = ref<Task[]>([])
+
+  const fetchTasks = async () => {
+    tasks.value = await api.task.getAll()
+  }
+
+  return {
+    tasks,
+    fetchTasks,
+  }
 })
