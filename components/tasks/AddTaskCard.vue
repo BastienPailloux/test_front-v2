c11d1f2ab35535f1f9439c19bd983f1238b0962b
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-02-13 15:25:18 +0100

    add the createTask in the store

--- components/tasks/AddTaskCard.vue
+++ components/tasks/AddTaskCard.vue
@@ -5,7 +5,11 @@
       Ajouter une tâche
     </p>
     <div v-else class="containerAddTask">
-      <input type="text" placeholder="Nom de la tâche" />
+      <input
+        type="text"
+        placeholder="Nom de la tâche"
+        v-model="newTaskTitle"
+      />
       <BaseButton @click="addTask">Créer</BaseButton>
     </div>
   </BaseCard>
@@ -15,14 +19,27 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
+import { useTasksStore } from '~/stores/tasks'
+
+const tasksStore = useTasksStore()
+
 const isCreating = ref(false)
+const newTaskTitle = ref('')
 
 const createNewTask = () => {
   isCreating.value = true
 }
 
-const addTask = () => {
-  isCreating.value = false
+const addTask = async () => {
+  try {
+    tasksStore.createTask({ title: newTaskTitle.value })
+    newTaskTitle.value = ''
+    isCreating.value = false
+  }
+  catch (error) {
+    // TODO: Handle error
+    console.error(error)
+  }
 }
 </script>
 
